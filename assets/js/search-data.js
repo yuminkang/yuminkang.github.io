2a97bdfc736832f71bf37d56417dca0f9145ca94
yuminkang <50940010+yuminkang@users.noreply.github.com>
2025-03-31 07:45:02 +0000

    Deploying to gh-pages from @ yuminkang/yuminkang.github.io@1e7f9bce1f85f326bc56c8def6eda71770342634 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "publications",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
